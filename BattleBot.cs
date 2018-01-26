a28859ea207c982e4e33c892fd3b8edeb0112b80
Adrian Banks <github@adrianbanks.co.uk>
2018-01-26 11:28:19 +0000

    Initial version

---
+++ BattleBot.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using Robocode;
+
+namespace ANB.BattleBot
+{
+    public class DestructoBot : Robot
+    {
+        public override void OnWin(WinEvent evnt)
+        {
+            for (int i = 0; i < 1080 / 30; i++)
+            {
+                TurnLeft(30);
+                TurnGunRight(30);
+            }
+        }
+
+        public override void Run()
+        {
+            BodyColor = Color.GreenYellow;
+            GunColor = Color.CornflowerBlue;
+            BulletColor = Color.Yellow;
+            RadarColor = Color.OrangeRed;
+            ScanColor = Color.Red;
+            
+            Back(300);
+            TurnLeft(Heading - 90);
+            //TurnGunRight(90);
+
+            while (true)
+            {
+                Ahead(150);
+                TurnRight(15);
+                TurnGunLeft(45);
+                Ahead(90);
+                TurnGunLeft(45);
+                TurnRight(15);
+                TurnGunLeft(45);
+                Back(300);
+                TurnGunLeft(45);
+                TurnRight(10);
+            }                
+        }
+
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(1);
+            Fire(1);
+        }
+    }
+}
---
+++ HackBot.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+using Robocode;
+using Robocode.RobotInterfaces.Peer;
+
+namespace ANB.BattleBot
+{
+    public class HackBot : Robot
+    {
+        public override void Run()
+        {
+            var peerField = GetType().GetField("peer", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var peer = (IBasicRobotPeer) peerField.GetValue(this);
+
+            Out.WriteLine("Peer: " + peer);
+            
+//            peerField.SetValue(this, new PeerProxy(peer, Out));
+        }
+
+/*
+        private class PeerProxy : RealProxy
+        {
+            private readonly IBasicRobotPeer peer;
+            private readonly TextWriter writer;
+
+            public PeerProxy(IBasicRobotPeer peer, TextWriter writer)
+            {
+                this.peer = peer;
+                this.writer = writer;
+            }
+
+            public override IMessage Invoke(IMessage msg)
+            {
+                IMethodCallMessage methodMessage = new MethodCallMessageWrapper((IMethodCallMessage) msg);
+                MethodBase method = methodMessage.MethodBase;
+
+                object returnValue;
+
+                writer.WriteLine("Calling: " + method.Name);
+                
+                if (method.Name == "GetEnergy")
+                {
+                    returnValue = 100.0;
+                }
+                else
+                {
+                    returnValue = method.Invoke(peer, methodMessage.Args);
+                }
+
+                return new ReturnMessage(returnValue, methodMessage.Args, methodMessage.ArgCount, methodMessage.LogicalCallContext, methodMessage);
+            }
+        }
+*/
+    }
+}
